555bddeb1d7af4f50f03263ad626dc02073c5ceb
JollyFoxy <pavel.mizirev@gmail.com>
2022-01-21 12:39:58 +0300

    LR02

---
+++ Ducks/Duck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ducks
+{
+    public abstract class Duck
+    {
+        public string Swim()
+        {
+            return "swim" + "" + GetType();
+        }
+        public abstract string Display();
+    }
+}
